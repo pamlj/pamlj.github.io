afd23c994b650b656d82c0bc54ca2f0d88265c94
mcfanda <mcfanda@gmail.com>
2024-03-19 18:05:39 +0100

    updates

---
+++ R/info.R
@@ -0,0 +1,22 @@
+# folders should be absolute or relative to the docssource folder
+# do not use the trailing slash
+MODULE_NAME="GAMLj"
+MODULE_FOLDER="~/Skinner/Forge/jamovi/pamlj/pamlj"
+PROJECT_FOLDER="~/Skinner/Forge/jamovi/pamlj/pamlj.github.io"
+SOURCE_FOLDER="docssource"
+
+DATALINK="https://github.com/pamlj/pamlj.github.io/blob/master/data"
+# These handle the release notes from commits
+MODULE_REPO="pamlj"
+MODULE_REPO_OWNER="pamlj"
+MODULE_LINK="https://github.com/pamlj/pamlj/"
+MODULE_EMAIL="mcfanda@gmail.com
+
+"
+
+FIRST_VERSION="Version.0.0.5"
+BANNED_COMMITS=list("initialize","fix commits","remove some marks")
+BANNED_COMMITS_GREP=list("^#","^!","^Merge branch","spelling","2.9.","2.9.","2.9.")
+
+# These handle the R vignettes
+
---
+++ R/newversion.R
@@ -0,0 +1,15 @@
+#remotes::install_github("mcfanda/mcdocs")
+library(mcdocs)
+mcdocs_init()
+
+## this renders the website
+#render_mcdocs()
+#bookdown::render_book("gitdocs/",output_dir = "../docs/book",clean = T)
+#pkgdown::build_site(pkg = "~/Skinner/Forge/jamovi/gamlj/gamlj")
+
+#cmd<-"mv ~/Skinner/Forge/jamovi/gamlj/gamlj/docs ~/Skinner/Forge/jamovi/gamlj/gamlj.github.io/docs/rpackage"
+#system(cmd)
+
+## this update your github osite
+update_git()
+
