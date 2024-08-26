619367a06d4bfda8391e4c94cce52771b6250e59
mcfanda <mcfanda@gmail.com>
2024-08-26 11:31:52 +0200

    updates

--- R/newversion.R
+++ R/newversion.R
@@ -1,14 +1,10 @@
 #remotes::install_github("mcfanda/mcdocs")
 library(mcdocs)
 mcdocs_init()
-render_mcdocs()
 
 ## this renders the website
-#bookdown::render_book("gitdocs/",output_dir = "../docs/book",clean = T)
-#pkgdown::build_site(pkg = "~/Skinner/Forge/jamovi/gamlj/gamlj")
-
-#cmd<-"mv ~/Skinner/Forge/jamovi/gamlj/gamlj/docs ~/Skinner/Forge/jamovi/gamlj/gamlj.github.io/docs/rpackage"
-#system(cmd)
+output_format = "rmdformats::readthedown"
+render_mcdocs(output_format)
 
 ## this update your github osite
 update_git()
